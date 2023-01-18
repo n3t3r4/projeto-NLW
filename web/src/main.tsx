445c62785ed18c0de34b8973c472264ceec27367
n3t3r4 <arthurleffa@gmail.com>
2023-01-18 10:48:23 -0300

    segunda aula / seed db error

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Habit } from './components/habits'
 import './index.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div>
+    <Habit completed={1}/>
+    <Habit completed={2}/>
+    <Habit completed={1}/>
+    <Habit completed={2}/>
+    </div>
   </React.StrictMode>,
 )
